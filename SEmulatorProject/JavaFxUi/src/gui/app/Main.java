a0ea62f8fa9574c4bfbb567fcfc06238d6e1a675
omrishtruzer <omrisht1999@outlook.com>
2025-09-09 17:41:28 +0300

    not working

--- SEmulatorProject/JavaFxUi/src/gui/app/Main.java
+++ SEmulatorProject/JavaFxUi/src/gui/app/Main.java
@@ -1,4 +1,4 @@
-package ui.app;
+package gui.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -16,7 +16,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
